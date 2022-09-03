ccd05cd038aae1206f35fbde3eb7d258fc370d58
whylone1y <108067513+whylone1y@users.noreply.github.com>
2022-09-03 17:39:56 +0600

    some changes

--- src/components/header/HeaderElements/Logo.jsx
+++ src/components/header/HeaderElements/Logo.jsx
@@ -2,21 +2,23 @@ import { GiBurningMeteor } from "react-icons/gi";
 
 export const Logo = () => {
   return (
-    <div className="flex items-center">
+    <div className="flex items-center w-[50%]">
       <GiBurningMeteor className="mr-2" />
       <h1 className="font-bold mr-6">Portfolio</h1>
-      <a
-        href=""
-        className="mr-6 hover:text-blue dark:hover:text-purple invisible lg:visible lg:text-xl"
-      >
-        Resume
-      </a>
-      <a
-        href=""
-        className="hover:text-blue dark:hover:text-purple invisible lg:visible lg:text-xl"
-      >
-        View Source
-      </a>
+      <div className="invisible lg:visible">
+        <a
+          href=""
+          className="mr-6 hover:text-blue dark:hover:text-purple lg:text-xl"
+        >
+          Resume
+        </a>
+        <a
+          href=""
+          className="hover:text-blue dark:hover:text-purple lg:text-xl"
+        >
+          View Source
+        </a>
+      </div>
     </div>
   );
 };
